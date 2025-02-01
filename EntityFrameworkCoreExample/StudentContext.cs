adb49a615cd0000c7d07c6d54354bc23ca072765
Uj-K <umkrall@gmail.com>
2025-02-01 00:28:13 -0800

    Configured DBContext class to use SQL Server

--- EntityFrameworkCoreExample/StudentContext.cs
+++ EntityFrameworkCoreExample/StudentContext.cs
@@ -7,10 +7,23 @@ using System.Threading.Tasks;
 
 namespace EntityFrameworkCoreExample
 {
+    // Entity Framework(EF)는 .NET 애플리케이션에서 데이터베이스와 상호 작용하는 ORM(Object-Relational Mapping)
+    // 프레임워크입니다. 이를 사용하면 데이터베이스 작업을 SQL 쿼리 없이 C# 코드만으로 수행할 수 있습니다.
     // EF Core getting started
     // https://learn.microsoft.com/en-us/ef/core/get-started/overview/install
-    internal class StudentContext : DbContext
+    public class StudentContext : DbContext
     {
+        public StudentContext()
+        {
+            
+        }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // database = The desired name for the database
+            // Server = The server we are connecting to. LocalDB is include with VS
+            // Trusted_Connection = Indicate that our windows account should be used
+            options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EFCoreExample;Trusted_Connection=True;");
+        }
     }
 }
