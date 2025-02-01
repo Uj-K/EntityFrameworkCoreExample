27540ce3b0a63364fed4f91e596c53738c5a8e17
Uj-K <umkrall@gmail.com>
2025-02-01 00:35:01 -0800

    Created a Student model to track in the database

--- EntityFrameworkCoreExample/StudentContext.cs
+++ EntityFrameworkCoreExample/StudentContext.cs
@@ -7,8 +7,10 @@ using System.Threading.Tasks;
 
 namespace EntityFrameworkCoreExample
 {
-    // Entity Framework(EF)는 .NET 애플리케이션에서 데이터베이스와 상호 작용하는 ORM(Object-Relational Mapping)
-    // 프레임워크입니다. 이를 사용하면 데이터베이스 작업을 SQL 쿼리 없이 C# 코드만으로 수행할 수 있습니다.
+    // Entity Framework(EF)는 .NET 애플리케이션에서 데이터베이스와 상호 작용하는
+    // ORM(Object-Relational Mapping) 프레임워크로    
+    // 데이터베이스 작업을 SQL 쿼리 없이 C# 코드만으로 수행할 수 있다.
+
     // EF Core getting started
     // https://learn.microsoft.com/en-us/ef/core/get-started/overview/install
     public class StudentContext : DbContext
