9461a2bd71eb69a94c540fad3c390f6c10d83f06
Uj-K <umkrall@gmail.com>
2025-01-31 22:53:09 -0800

    Added EF Core SQL Server Package and created the database context class

--- EntityFrameworkCoreExample/StudentContext.cs
+++ EntityFrameworkCoreExample/StudentContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace EntityFrameworkCoreExample
 {
-    internal class StudentContext
+    // EF Core getting started
+    // https://learn.microsoft.com/en-us/ef/core/get-started/overview/install
+    internal class StudentContext : DbContext
     {
+
     }
 }
